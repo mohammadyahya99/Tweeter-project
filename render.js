6b228c651f9e3cef042ece16c4b1929d510438ae
MohammadYahya <mohamadyahya979@gmail.com>
2022-07-14 12:16:54 -0700

    second commit

--- render.js
+++ render.js
@@ -3,21 +3,22 @@ const render = function (posts) {
   $(".container").empty();
 
   var ctag = `<div class="creatComment">
-    <input type="text" />
-    <button>comment</button>
+    <input class="commentinput" type="text" />
+    <button class="commentbutton">comment</button>
  </div>
  <button class="delete">delete post</button>`;
 
   for (let index of posts) {
     let postTag = `<div class="posts">
-<div id="${index.id}">${index.text}</div>
+   <div class="title"> ${index.text}</div>
+<div class="dbutton" id="${index.id}"></div>
 ${ctag}
 
     </div>`;
     post.append(postTag);
     for (let myloop of index.comments) {
       $(`#${index.id}`).append(
-        `<div class="comments"> <p>${myloop.text}</p> </div> `
+        `<div id="${myloop.id}"class="comments"><i class="material-icons">delete</i> <p>${myloop.text}</p> </div> `
       );
     }
   }
